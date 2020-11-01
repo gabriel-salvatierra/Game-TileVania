006ffb378fa997a1e79981d0722cf2f7d73ac84a
angelsalvatierra <angel.salvatierra@globallogic.com>
2020-11-01 18:00:57 -0300

    Player Run and Jump

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour {
+
+    // Config
+    [SerializeField] float runSpeed = 7f;
+    [SerializeField] float jumpSpeed = 7f;
+
+    // State
+    bool isAlive = true;
+
+    // Cached component references
+    Rigidbody2D myRigidBody;
+    Animator myAnimator;
+    Collider2D myCollider2D;
+
+    // Start is called before the first frame update
+    void Start() {
+        myRigidBody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        myCollider2D = GetComponent<Collider2D>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        Run();
+        Jump();
+        FlipSprite();
+    }
+
+    private void Run() {
+        float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal"); // value is between -1 to +1
+        Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRigidBody.velocity.y);
+        myRigidBody.velocity = playerVelocity;
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+        myAnimator.SetBool("Running", playerHasHorizontalSpeed);
+    }
+
+    private void Jump() {
+        if (!myCollider2D.IsTouchingLayers(LayerMask.GetMask("Ground"))) {
+            return;
+        }
+        if (CrossPlatformInputManager.GetButtonDown("Jump")) {
+            Vector2 jumpVelocityToAdd = new Vector2(0f, jumpSpeed);
+            myRigidBody.velocity += jumpVelocityToAdd;
+        }
+    }
+
+    private void FlipSprite() {
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+        if (playerHasHorizontalSpeed) {
+            transform.localScale = new Vector2(Mathf.Sign(myRigidBody.velocity.x), 1f);
+        }
+    }
+
+}
