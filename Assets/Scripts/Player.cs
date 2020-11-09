01bd799f738ee7a561de23b6dbadcb0b4716dfb8
angelsalvatierra <angel.salvatierra@globallogic.com>
2020-11-08 21:25:30 -0300

    Hazards Layer

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -79,7 +79,7 @@ public class Player : MonoBehaviour {
     }
 
     private void Die() {
-        if (myBodyCollider.IsTouchingLayers(LayerMask.GetMask("Enemy"))) {
+        if (myBodyCollider.IsTouchingLayers(LayerMask.GetMask("Enemy", "Hazard"))) {
             isAlive = false;
             myAnimator.SetTrigger("Dying");
             myRigidBody.velocity = deathKick;
